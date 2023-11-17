a467c160d6e862fe4dbf125f6e1d2002e3239082
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-17 18:57:25 -0300

    add supplier example in FI_from_API

--- src/FI_from_API.java
+++ src/FI_from_API.java
@@ -1,10 +1,13 @@
+import java.time.LocalTime;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class FI_from_API {
     public static void main(String[] args) {
         FI_from_API fiAPI = new FI_from_API();
         fiAPI.predicate();
+        fiAPI.supplier();
     }
 
     public void predicate() {
@@ -17,5 +20,18 @@ public class FI_from_API {
         // boolean test(T t, U u)
         BiPredicate<String, Integer> checkLength = (str, len) -> str.length() == len;
         System.out.println(checkLength.test("Vatican City", 8));//false (length is 12)
+    }
+
+    public void supplier() {
+        // Supplier<T> is a functional interface i.e. one abstract method:
+        // T get()
+        Supplier<StringBuilder> supSB = () -> new StringBuilder();
+        System.out.println("Supplier SB: " + supSB.get().append("SK"));//Supplier SB: SK
+
+        Supplier<LocalTime> supTime = () -> LocalTime.now();
+        System.out.println("Supplier time: " + supTime.get());//Supplier time: the time now
+
+        Supplier<Double> sRandom = () -> Math.random();
+        System.out.println(sRandom.get());//e.g. 0.8453940688463555
     }
 }
