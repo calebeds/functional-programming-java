32d4b9d3cea02d5ab91122083ca56026867cd303
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-19 11:37:00 -0300

    add constructor method reference example

--- src/MethodReferenceTypes.java
+++ src/MethodReferenceTypes.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.*;
 
 public class MethodReferenceTypes {
@@ -10,6 +7,7 @@ public class MethodReferenceTypes {
         boundMethodReference();
         unboundMethodReferences();
         staticMethodReferences();
+        constructorMethodReference();
     }
 
     public static void boundMethodReference() {
@@ -75,6 +73,37 @@ public class MethodReferenceTypes {
         listOfNumbers = Arrays.asList(8, 12, 4, 3, 7);
         sortMR.accept(listOfNumbers);//execution
         System.out.println(listOfNumbers); //[3, 4, 7, 8, 12]
+
+    }
+
+    public static void constructorMethodReference() {
+        // Supplier<T>
+        // T get()
+        Supplier<StringBuilder> sbL = () -> new StringBuilder(); //lambda
+        Supplier<StringBuilder> sbMR = StringBuilder::new; //method reference
+
+        StringBuilder sb1 = sbL.get();
+        sb1.append("lambda version");
+        System.out.println(sb1);
+
+        StringBuilder sb2 = sbMR.get();
+        sb2.append("method reference version");
+        System.out.println(sb2);
+
+        // Function<T, R>
+        // R apply(T)
+        // List<String> apply(Integer)
+        // ArrayList(int initialCapacity)
+
+        Function<Integer, List<String>> alL = x -> new ArrayList<>(x);
+        Function<Integer, List<String>> alMR = ArrayList::new;
+        List<String> ls1 = alL.apply(10); //size 10
+        ls1.add("21");
+        System.out.println(ls1); //[21]
+
+        List<String> ls2 = alMR.apply(8); //size 8
+        ls2.add("88");
+        System.out.println(ls2); //[88]
 
     }
 
