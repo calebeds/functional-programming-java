295fd922364ba7345820dea97f479f7b706b81cc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-18 22:52:06 -0300

    add unbound method references example

--- src/MethodReferenceTypes.java
+++ src/MethodReferenceTypes.java
@@ -1,3 +1,5 @@
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -5,6 +7,7 @@ public class MethodReferenceTypes {
 
     public static void main(String[] args) {
         boundMethodReference();
+        unboundMethodReferences();
     }
 
     public static void boundMethodReference() {
@@ -29,6 +32,30 @@ public class MethodReferenceTypes {
 
         System.out.println(titleL.test("Mr. "));//true
         System.out.println(titleMr.test("Ms. "));//false
+    }
+
+    public static void unboundMethodReferences() {
+        // Function<T, R>
+        // R apply(T)
+        // String apply(String)
+        Function<String, String> upperL = s -> s.toUpperCase();
+        Function<String, String> upperMR = String::toUpperCase;
+        // The function is unbound, so you need to specify which instance to call it on
+        System.out.println(upperL.apply("sean")); //SEAN
+        System.out.println(upperMR.apply("sean")); //SEAN
+
+        // BiFunction<T, U, R>
+        // R apply(T t, U u)
+        // String apply(String, String)
+        BiFunction<String, String, String> concatL = (s1, s2) -> s1.concat(s2);
+        BiFunction<String, String, String> concatMR = String::concat;
+        System.out.println(concatL.apply("Sean ", "Kennedy")); //Sean Kennedy
+
+        // 1st parameter is used for executing the instance method
+        // "Sean".concat("Kennedy")
+        System.out.println(concatMR.apply("Sean ", "Kennedy"));
+
+
     }
 
 }
