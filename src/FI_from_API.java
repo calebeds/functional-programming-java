ad85388fb81f25ede7dfd7b7fea4043f116de904
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-18 21:25:34 -0300

    add consumer example

--- src/FI_from_API.java
+++ src/FI_from_API.java
@@ -1,13 +1,15 @@
 import java.time.LocalTime;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.*;
 
 public class FI_from_API {
     public static void main(String[] args) {
         FI_from_API fiAPI = new FI_from_API();
         fiAPI.predicate();
         fiAPI.supplier();
+        fiAPI.consumer();
     }
 
     public void predicate() {
@@ -33,5 +35,31 @@ public class FI_from_API {
 
         Supplier<Double> sRandom = () -> Math.random();
         System.out.println(sRandom.get());//e.g. 0.8453940688463555
+    }
+
+    public void consumer() {
+        // Consumer<T> is a functional interface i.e. one abstract method:
+        // void accept(T t)
+        Consumer<String> printC = s -> System.out.println(s);
+        printC.accept("To be or not to be, that is the question");
+
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Mary");
+        names.forEach(printC);
+
+        // BiConsumer<T, U> is a functional interface i.e. one abstract method:
+        // void accept(T t, U u)
+        var mapCapitalCities = new HashMap<String, String>();
+        // Note: The return value of put(k,v) is just ignored (and not returned from the lambda)
+        BiConsumer<String, String> biCon = (key, value) -> mapCapitalCities.put(key, value);
+        biCon.accept("Dublin", "Ireland");
+        biCon.accept("Washington D.C.", "USA");
+
+        System.out.println(mapCapitalCities);
+
+        BiConsumer<String, String> mapPrint = (key, value) -> System.out.println(key + " is the capital of: " + value);
+        mapCapitalCities.forEach(mapPrint);
+
     }
 }
